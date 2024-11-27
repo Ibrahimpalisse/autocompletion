13b1440d874e1d81196f7ef901d9551e670aad36
VIGNES_Ibrahim <168725336+Ibrahimpalisse@users.noreply.github.com>
2024-11-27 09:30:31 +0100

    Premier commit

---
+++ src/Models/RecupeSearch.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Models;
+require_once __DIR__ . './../config/bdd.php';
+
+class RecupeSearch{
+     
+    private $pdo;
+
+
+    public function __construct(){
+        $this->pdo;
+    }
+    
+
+}
